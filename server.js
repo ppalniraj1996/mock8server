34b6b9d9df53b67051f7d6c416758754d7321d1f
ppalniraj1996 <97506350+ppalniraj1996@users.noreply.github.com>
2022-08-18 15:22:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
-server.use("/", router);
+server.use("/api", router);
 server.listen(PORT, () => console.log("Running on port 3000"));
